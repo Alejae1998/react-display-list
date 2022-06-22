793a430675b21fd1dff9484381c0d9ed3807616d
Alejae1998 <alejagr1998@gmail.com>
2022-06-21 17:06:54 -0700

    fixing

--- src/PetsItem.js
+++ src/PetsItem.js
@@ -2,8 +2,8 @@ export default function Pet({ pet }) {
   return (
     <div className="pets">
       <h2>Breed: {pet.breed}</h2>
-      <p>Location: {pet.cuteness}</p>
-      <p>Rating: {pet.size}</p>
+      <p>Cuteness level: {pet.cuteness}</p>
+      <p>Size: {pet.size}</p>
     </div>
   );
 }
